66ac41f9d1021436f78a3d705e4aaebf7c69b7aa
Harshala Ramachandra <Harshala.R@philips.com>
2023-08-07 10:16:43 +0530

    Trial3

--- misaligned.cpp
+++ misaligned.cpp
@@ -4,14 +4,13 @@
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0, counter=0;
+    int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-			counter++;
+            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
         }
     }
-    return counter;
+    return i * j;
 }
 
 int main() {
